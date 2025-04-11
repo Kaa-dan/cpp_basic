36dcb2aa82b6d6d21f7a947bde06ea614da1b279
kaa-dan <nithinraj.dev@gmail.com>
2025-04-11 15:23:30 +0530

    type conversion

---
+++ typeconversion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    /*type conversion = conversion a value of one data type to another
+                            implicit = automatic
+                            explicit = precede value with new data type (int)*/
+
+    int x = 2.5;
+    std::cout << x << std::endl;
+
+    double b = (int)3.15;
+
+    std::cout << b;
+    
+    
+}
