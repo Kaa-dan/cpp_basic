7f3b71ff03c562219f6331fd5bfebf9b10009953
kaa-dan <nithinraj.dev@gmail.com>
2025-05-20 19:04:46 +0530

    overloaded functions

--- function.cpp
+++ function.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 
-void indianScammer(std::string name);
+int indianScammer(std::string name);
 int main()
 {
 
     // function = a block of reusable code
     std::string name = " nithin raj s";
-    indianScammer(name);
+    int val = indianScammer(name);
+
+    std::cout << val;
     return 0;
 }
 
-void indianScammer(std::string name)
+int indianScammer(std::string name)
 {
     std::cout << "Sir this is json from microsoft" << name << std::endl;
     std::cout << "Sar OTP";
+    return 3;
 };
---
+++ overloaded-function.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void bakePizza();
+void bakePizza(std::string topping);
+
+int main()
+{
+
+    bakePizza();
+    bakePizza("wood");
+    return 0;
+}
+
+void bakePizza()
+{
+    std::cout<< "Here is your pizza \n";
+}
+
+void bakePizza(std::string topping){
+     std::cout<< "Here is your " << topping << " pizza \n"; 
+};
