7bdaffec556cae55e664de847657cff9a38128ac
kaa-dan <nithinraj.dev@gmail.com>
2025-05-21 13:45:21 +0530

    array functiona and poiter

--- foreachloop.cpp
+++ foreachloop.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+double getTotal(double prices[], int size);
+
 int main()
 {
     std::string students[] = {"nithin", "abhirami"};
@@ -12,9 +14,28 @@ int main()
     // for each loop
 
     int grades[] = {40, 50, 60};
+
     for (int grade : grades)
     {
         std::cout << grade << std::endl;
     }
+
+    // passing function
+
+    double prices[] = {1.2, 21.2, 2.2};
+    int size = sizeof(prices) / sizeof(prices[0]);
+    double total = getTotal(prices, size);
+
+    std::cout << "total is " << total << std::endl;
     return 0;
+}
+
+double getTotal(double prices[], int size)
+{
+    double total = 0;
+    for (int i = 0; i < size; i++)
+    {
+        total += prices[i];
+    }
+    return total;
 }
