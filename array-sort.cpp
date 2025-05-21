27e47b856050af99db653cd8b337202b0b42bb5c
kaa-dan <nithinraj.dev@gmail.com>
2025-05-21 15:12:22 +0530

    bubble sort

---
+++ array-sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void sort(int array[], int size);
+
+int main()
+{
+
+    int array[] = {3, 25, 2, 52, 53};
+    int size = sizeof(array) / sizeof(array[0]);
+    sort(array, size);
+
+    for (int value : array)
+    {
+        std::cout << value << " ";
+    }
+    return 0;
+}
+
+void sort(int array[], int size)
+{
+    int temp;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
