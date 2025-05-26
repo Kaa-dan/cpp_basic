5acc0d10f16cd1d8c318cba6a862de0f089596ce
kaa-dan <nithinraj.dev@gmail.com>
2025-05-26 18:31:33 +0530

    funciton template

---
+++ function-template.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    /* function template = describes what a function looks like.ab
+    can be used to generate as many overloaded functions as needed,
+    each using different data types
+    */
+
+    
+    return 0;
+}
