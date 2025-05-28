ca73d51a13b202ddb33018bde2e806608d3ffc7d
kaa-dan <nithinraj.dev@gmail.com>
2025-05-28 14:49:27 +0530

    getters and setters

---
+++ getters-setters.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Stove
+{
+public:
+    int getTemparature()
+    {
+        return temparature;
+    }
+    void setTemparature(int temparature)
+    {
+        this->temparature = temparature;
+    }
+
+private:
+    int temparature;
+};
+int main()
+{
+
+    // abstration = hiding unnecessary data from outside a class
+    // getter = function that makes a private attribute READABLE
+    // setter = function that makes a private attribute WRITABLE
+
+    return 0;
+}
