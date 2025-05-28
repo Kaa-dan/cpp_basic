7072d26fdac946345c7f98d3a5bf54e9c755c5bb
kaa-dan <nithinraj.dev@gmail.com>
2025-05-28 14:42:03 +0530

    overloaded constructors;

--- constructor.cpp
+++ constructor.cpp
@@ -4,11 +4,17 @@ class Student
 public:
     std::string name;
     int age;
-
+    bool status;
     Student(std::string name, int x)
     {
         this->name = name;
         age = x;
+    };
+    Student(std::string name, int x, bool y)
+    {
+        this->name = name;
+        age = x;
+        status = y;
     }
 };
 int main()
@@ -16,6 +22,11 @@ int main()
 
     /*constructor = special method that is automatically called when an object is instantiated useful
     for assigning values to attributes as arguments*/
+
+    /* overloaded constructors  = multiple constructors w/ same name but different parameters
+    allows for varying arguments when instantiating an object*/
+
     Student student1("nithin", 11);
+    Student student2("ied", 12, false);
     return 0;
 }
