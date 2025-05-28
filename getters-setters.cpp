dd38bbd7c2b0c86f61f21085d5656a33cafa9303
kaa-dan <nithinraj.dev@gmail.com>
2025-05-28 15:26:46 +0530

    course done

--- getters-setters.cpp
+++ getters-setters.cpp
@@ -21,6 +21,6 @@ int main()
     // abstration = hiding unnecessary data from outside a class
     // getter = function that makes a private attribute READABLE
     // setter = function that makes a private attribute WRITABLE
-
+   
     return 0;
 }
